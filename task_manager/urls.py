d8e40f98061c3ad7671cc9c08d13e620f4cc015c
VolodimirV <vladimir_veko@mail.ru>
2023-03-13 16:02:20 +0300

    modified task_manager/urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
 from main.admin import task_manager_admin_site
+from main.views import UserViewSet, TagViewSet, TaskViewSet
+
+router = routers.SimpleRouter()
+router.register(r"users", UserViewSet, basename="users")
+router.register(r"tags", TagViewSet, basename="tags")
+router.register(r"tasks", TaskViewSet, basename="tasks")
 
 urlpatterns = [
     path("admin/", task_manager_admin_site.urls),
+    path("api/", include(router.urls)),
 ]
