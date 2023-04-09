427799ae7bda9e9856392d26d1609e05791284fa
VolodimirV <vladimir_veko@mail.ru>
2023-04-09 18:46:02 +0300

    modified 09.04.23

--- test/test_permissions.py
+++ test/test_permissions.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from main.models import User
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 from main.models import Task
